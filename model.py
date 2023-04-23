4c7e537cfed44c2155fed3bf32e0189acdcebed0
runtome <suphot.n@gmail.com>
2023-04-23 10:08:25 +0700

    Modity Model (but it nowork)

--- model.py
+++ model.py
@@ -1,11 +1,12 @@
 from pymongo import MongoClient
 
 def asking_people(mangocluster):
-    client = MongoClient(mangocluster)
+    client = MongoClient(mangocluster,connectTimeoutMS=30000, socketTimeoutMS=None, connect=False, maxPoolsize=1)
     db = client.bus
     data_in = db.bussituation
     results = data_in.find().sort('timestamp',-1).limit(1)
-    
+    print(db)
+    print(data_in)
     qty = []
     
     for result in results:
